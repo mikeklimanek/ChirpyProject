709c2c094db0a3d7091afc6bc6eff0554de89465
mikeklimanek <mike.klimanek@gmail.com>
2023-12-31 00:20:27 +0100

    added a metric system to count visits, also added a reset for metrics

--- main.go
+++ main.go
@@ -3,19 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := &apiConfig{}
 	mux := http.NewServeMux()
 	mux.Handle("/healthz", http.HandlerFunc(healthzHandler))
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))) 
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
+	mux.Handle("/metrics", http.HandlerFunc(apiCfg.metricsHandler))
+	mux.Handle("/reset", http.HandlerFunc(apiCfg.resetHandler))
 	corsMux := middlewareCors(mux)
 
 	srv := &http.Server{
-		Addr:   ":" + port,
+		Addr:  ":" + port,
 		Handler: corsMux,
 	}
 
@@ -37,12 +45,25 @@ func middlewareCors(next http.Handler) http.Handler {
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
-	// HEADER
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	// STATUS
 	w.WriteHeader(http.StatusOK)
-
-	// BODY
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + strconv.Itoa(cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
 }
